d7eb60d0ec9cf0701c4097ef0e900083805a3c4e
Admin <Admin>
2016-10-08 18:37:23 -0700

    Assignment2 Commit

--- Assignment_2/ClientList.js
+++ Assignment_2/ClientList.js
@@ -1,56 +1,53 @@
 {
-   "Root": {
-      "Row": [
-         {
-            "Last_Name": "Johnson",
-            "First_Name": "Frank",
-            "Phone_Number": "425-251-1234",
-            "Annual_Sales": "30000",
-            "Commissions": "3750"
-         },
-         {
-            "Last_Name": "Davis",
-            "First_Name": "John",
-            "Phone_Number": "425-112-2345",
-            "Annual_Sales": "45000",
-            "Commissions": "5625"
-         },
-         {
-            "Last_Name": "Frederick",
-            "First_Name": "David",
-            "Phone_Number": "206-219-3248",
-            "Annual_Sales": "1215",
-            "Commissions": "151.875"
-         },
-         {
-            "Last_Name": "Rice",
-            "First_Name": "Fred",
-            "Phone_Number": "503-390-3341",
-            "Annual_Sales": "115318",
-            "Commissions": "14414.75"
-         },
-         {
-            "Last_Name": "Todd",
-            "First_Name": "Ricky",
-            "Phone_Number": "428-222-1245",
-            "Annual_Sales": "11135",
-            "Commissions": "1391.875"
-         },
-         {
-            "Last_Name": "Hoffman",
-            "First_Name": "Ted",
-            "Phone_Number": "428-111-1245",
-            "Annual_Sales": "59787",
-            "Commissions": "7473.375"
-         },
-         {
-            "Last_Name": "Brown",
-            "First_Name": "Bob",
-            "Phone_Number": "425-110-0011",
-            "Annual_Sales": "54879415",
-            "Commissions": "6859926.875"
-         }
-      ],
-      "_xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance"
-   }
+  "data": [
+	 {
+		"Last_Name": "Johnson",
+		"First_Name": "Frank",
+		"Phone_Number": "425-251-1234",
+		"Annual_Sales": "30000",
+		"Commissions": "3750"
+	 },
+	 {
+		"Last_Name": "Davis",
+		"First_Name": "John",
+		"Phone_Number": "425-112-2345",
+		"Annual_Sales": "45000",
+		"Commissions": "5625"
+	 },
+	 {
+		"Last_Name": "Frederick",
+		"First_Name": "David",
+		"Phone_Number": "206-219-3248",
+		"Annual_Sales": "1215",
+		"Commissions": "151.875"
+	 },
+	 {
+		"Last_Name": "Rice",
+		"First_Name": "Fred",
+		"Phone_Number": "503-390-3341",
+		"Annual_Sales": "115318",
+		"Commissions": "14414.75"
+	 },
+	 {
+		"Last_Name": "Todd",
+		"First_Name": "Ricky",
+		"Phone_Number": "428-222-1245",
+		"Annual_Sales": "11135",
+		"Commissions": "1391.875"
+	 },
+	 {
+		"Last_Name": "Hoffman",
+		"First_Name": "Ted",
+		"Phone_Number": "428-111-1245",
+		"Annual_Sales": "59787",
+		"Commissions": "7473.375"
+	 },
+	 {
+		"Last_Name": "Brown",
+		"First_Name": "Bob",
+		"Phone_Number": "425-110-0011",
+		"Annual_Sales": "54879415",
+		"Commissions": "6859926.875"
+	 }
+  ]
 }
